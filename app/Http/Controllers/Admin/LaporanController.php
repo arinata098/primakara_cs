a57e0124c799bf971eff49b3a5db19eff9f25dfb
Bagus Arinata <arinata@primakara.ac.id>
2023-10-02 12:49:27 +0800

    check controller 3 days

--- app/Http/Controllers/Admin/LaporanController.php
+++ app/Http/Controllers/Admin/LaporanController.php
@@ -11,6 +11,7 @@ use App\Models\Validasi;
 use App\Models\AtributChecklist;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
 use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
+use PhpOffice\PhpSpreadsheet\Writer\Csv;
 
 
 class LaporanController extends Controller
@@ -21,66 +22,72 @@ class LaporanController extends Controller
         $tanggalAwal = $request->filled('tanggal_awal') ? $request->input('tanggal_awal') : null;
         $tanggalAkhir = $request->filled('tanggal_akhir') ? $request->input('tanggal_akhir') : null;
 
-        $results = Validasi::select(
-            'validasi_data.id_atribut_checklist',
-            'validasi_data.tgl_check',
-            'validasi_data.id_cs',
-            'data_atribut_checklist.id_ruangan',
-            'ruangan.nama_ruangan',
-            'data_atribut_checklist.id_list',
-            'check_list.nama_list',
-            'data_atribut_checklist.status',
-            'validasi_data.validasi'
-        )
-            ->join('data_atribut_checklist', 'validasi_data.id_atribut_checklist', '=', 'data_atribut_checklist.id_atribut')
-            ->join('ruangan', 'data_atribut_checklist.id_ruangan', '=', 'ruangan.id_ruangan')
-            ->join('check_list', 'data_atribut_checklist.id_list', '=', 'check_list.id_list')
-            ->when($tanggalAwal && $tanggalAkhir, function ($query) use ($tanggalAwal, $tanggalAkhir) {
-                // Tambahkan kondisi untuk memfilter berdasarkan rentang tanggal
-                $query->whereBetween('validasi_data.tgl_check', [$tanggalAwal, $tanggalAkhir]);
-            })
-            ->orderBy('data_atribut_checklist.id_ruangan')
-            ->orderBy('validasi_data.tgl_check')
-            ->orderBy('data_atribut_checklist.id_list')
-            ->get();
+        // Mengecek apakah filter tanggal telah digunakan
+        $filterUsed = $request->filled('tanggal_awal') && $request->filled('tanggal_akhir');
 
         // Inisialisasi variabel untuk menyimpan data yang sudah dielompokkan
         $groupedData = [];
 
-        foreach ($results as $item) {
-            $ruangan = $item->nama_ruangan;
-            $petugas = $item->user->username;
-            $list = $item->nama_list;
-            $tgl_check = $item->tgl_check;
-            $status = $item->status;
-        
-            if (!isset($groupedData[$ruangan])) {
-                $groupedData[$ruangan] = [
-                    'petugas' => $petugas,
-                    'tugas' => [],
-                    'tanggal' => [], // Tambahkan array untuk tanggal
-                ];
+        $data = $request->all();
+
+        if ($data){
+
+            $results = Validasi::select(
+                'validasi_data.id_atribut_checklist',
+                'validasi_data.tgl_check',
+                'validasi_data.id_cs',
+                'data_atribut_checklist.id_ruangan',
+                'ruangan.nama_ruangan',
+                'data_atribut_checklist.id_list',
+                'check_list.nama_list',
+                'data_atribut_checklist.status',
+                'validasi_data.validasi'
+            )
+                ->join('data_atribut_checklist', 'validasi_data.id_atribut_checklist', '=', 'data_atribut_checklist.id_atribut')
+                ->join('ruangan', 'data_atribut_checklist.id_ruangan', '=', 'ruangan.id_ruangan')
+                ->join('check_list', 'data_atribut_checklist.id_list', '=', 'check_list.id_list')
+                ->when($tanggalAwal && $tanggalAkhir, function ($query) use ($tanggalAwal, $tanggalAkhir) {
+                    // Tambahkan kondisi untuk memfilter berdasarkan rentang tanggal
+                    $query->whereBetween('validasi_data.tgl_check', [$tanggalAwal, $tanggalAkhir]);
+                })
+                ->orderBy('data_atribut_checklist.id_ruangan')
+                ->orderBy('validasi_data.tgl_check')
+                ->orderBy('data_atribut_checklist.id_list')
+                ->get();
+
+            foreach ($results as $item) {
+                $ruangan = $item->nama_ruangan;
+                $petugas = $item->user->username;
+                $list = $item->nama_list;
+                $tgl_check = $item->tgl_check;
+                $status = $item->status;
+
+                if (!isset($groupedData[$ruangan])) {
+                    $groupedData[$ruangan] = [
+                        'petugas' => $petugas,
+                        'tugas' => [],
+                        'tanggal' => [], // Tambahkan array untuk tanggal
+                    ];
+                }
+
+                if (!isset($groupedData[$ruangan]['tugas'][$list])) {
+                    $groupedData[$ruangan]['tugas'][$list] = [];
+                }
+
+                if (!isset($groupedData[$ruangan]['tanggal'][$tgl_check])) {
+                    $groupedData[$ruangan]['tanggal'][$tgl_check] = $tgl_check; // Tambahkan tanggal ke array tanggal
+                }
+
+                // Menambahkan status ke dalam tugas yang sesuai
+                if (!isset($groupedData[$ruangan]['tugas'][$list][$tgl_check])) {
+                    $groupedData[$ruangan]['tugas'][$list][$tgl_check] = 0; // Default value jika tidak ada status
+                }
+
+                // $groupedData[$ruangan]['tugas'][$list][$tgl_check] += $status; //untuk menjumlahkan jika ada yang sama di hari yang sama maka status akan di jumlahkan semua
+                $groupedData[$ruangan]['tugas'][$list][$tgl_check] = $status;
             }
-        
-            if (!isset($groupedData[$ruangan]['tugas'][$list])) {
-                $groupedData[$ruangan]['tugas'][$list] = [];
-            }
-        
-            if (!isset($groupedData[$ruangan]['tanggal'][$tgl_check])) {
-                $groupedData[$ruangan]['tanggal'][$tgl_check] = $tgl_check; // Tambahkan tanggal ke array tanggal
-            }
-        
-            // Menambahkan status ke dalam tugas yang sesuai
-            if (!isset($groupedData[$ruangan]['tugas'][$list][$tgl_check])) {
-                $groupedData[$ruangan]['tugas'][$list][$tgl_check] = 0; // Default value jika tidak ada status
-            }
-        
-            // $groupedData[$ruangan]['tugas'][$list][$tgl_check] += $status; //untuk menjumlahkan jika ada yang sama di hari yang sama maka status akan di jumlahkan semua
-            $groupedData[$ruangan]['tugas'][$list][$tgl_check] = $status;
+
         }
-        
-        // Mengecek apakah filter tanggal telah digunakan
-        $filterUsed = $request->filled('tanggal_awal') && $request->filled('tanggal_akhir');
 
         return view('admin.laporan.index', [
             'title' => 'Laporan',
@@ -155,6 +162,8 @@ class LaporanController extends Controller
             $groupedData[$ruangan]['tugas'][$list][$tgl_check] = $status;
         }
 
+        // dd($groupedData);
+
         return collect($groupedData);
     }
 
@@ -183,9 +192,22 @@ class LaporanController extends Controller
             // Judul kolom
             $sheet->setCellValueByColumnAndRow(1, $rowIndex, 'List Pekerjaan');
             $colIndex = 2;
+            $iteration = 0;
+
             foreach ($ruanganData['tanggal'] as $tanggal) {
+                if ($iteration === 3) {
+                    $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, 'Controller');
+                    $colIndex++;
+                    $iteration = 0;
+                }
+
                 $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, $tanggal);
                 $colIndex++;
+                $iteration++;
+            }
+            // Menambahkan "Validation" setelah selesai perulangan
+            if ($iteration > 0 && $iteration < 3) {
+                $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, 'Controller');
             }
             $rowIndex++;
 
@@ -193,13 +215,26 @@ class LaporanController extends Controller
             foreach ($ruanganData['tugas'] as $list => $listData) {
                 $sheet->setCellValueByColumnAndRow(1, $rowIndex, $list);
                 $colIndex = 2;
+                $iteration = 0;
+
                 foreach ($ruanganData['tanggal'] as $tanggal) {
+                    if ($iteration === 3) {
+                        $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, '✓');
+                        $colIndex++;
+                        $iteration = 0;
+                    }
+
                     if (isset($listData[$tanggal])) {
-                        $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, $listData[$tanggal]);
+                        $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, '✓');
                     } else {
                         $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, '');
                     }
                     $colIndex++;
+                    $iteration++;
+                }
+                // Menambahkan "Validation" setelah selesai perulangan
+                if ($iteration > 0 && $iteration < 3) {
+                    $sheet->setCellValueByColumnAndRow($colIndex, $rowIndex, '✓');
                 }
                 $rowIndex++;
             }
@@ -219,5 +254,5 @@ class LaporanController extends Controller
         $writer->save('php://output');
     }
 
-    
+
 }
